be746150650de3469ca8bf0595a97fdc2825781f
Alvi Syahri <me@alvisyahri.com>
2018-10-08 20:42:58 +0700

    update template, datatable, jdbc, sqljdbc

---
+++ rekonsilasi/src/main/java/com/rekonsiliasi/JstlView.java
@@ -0,0 +1,23 @@
+package com.rekonsiliasi;
+
+import java.util.*;
+
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.view.InternalResourceView;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JstlView extends InternalResourceView {
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String dispatcherPath = prepareForRendering(request, response);
+		// set original view being asked for as a request parameter
+		request.setAttribute("partial", "../../WEB-INF/pages/" +dispatcherPath.substring(dispatcherPath.lastIndexOf("/") + 1));
+
+		// force everything to be template.jsp
+		RequestDispatcher rd = request.getRequestDispatcher("/static/layout/template.jsp");
+		rd.include(request, response);
+	}
+}
--- rekonsilasi/src/main/java/com/rekonsiliasi/MainController.java
+++ rekonsilasi/src/main/java/com/rekonsiliasi/MainController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -20,9 +21,10 @@ public class MainController {
 	return "redirect:login";
     }
 
-    @RequestMapping(value = "/home")
-    public String asd() {
-    	return "index";
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView handleRequest() {
+    
+    return new ModelAndView("index");
     }
 	
 }
